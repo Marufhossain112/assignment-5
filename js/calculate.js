45da457def2da37d959484b32d7354761385fd5b
maruf <lancerboy284@gmail.com>
2022-08-22 21:45:28 +0600

    some neccessary comments added

--- js/calculate.js
+++ js/calculate.js
@@ -1,8 +1,10 @@
+// function for calculating player expenses
 function perPlayer(selectedPlayersNumber) {
     const playerPrice = document.getElementById('player-price').value;
     const perPlayerTotal = selectedPlayersNumber * playerPrice; 
     return perPlayerTotal;
 }
+// function for getting value from an input id
 function getValueFromId(elementId) {
     const elementValueString = document.getElementById(elementId).value;
     const elementValueNumber = parseInt(elementValueString);
--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const selectBtn1 = document.getElementsByClassName("select-btn");
-for (const button of selectBtn1) {
-  const playerList = document.getElementById("selected-players-list");
+// get all buttons
+const selectBtn = document.getElementsByClassName("select-btn");
+for (const button of selectBtn) {
   const noSelection = document.getElementById("no-selection");
+  const playerList = document.getElementById("selected-players-list");
   button.addEventListener("click", function () {
     noSelection.style.display = "none";
     const parentElementLength = playerList.childNodes.length;
+    // if you try to add more than five players it will show an error
     if (parentElementLength > 4) {
       alert("You can't add more players.");
     } else {
@@ -12,6 +14,7 @@ for (const button of selectBtn1) {
       const playerElement = document.createElement("li");
       playerElement.innerText = playerText;
       playerList.append(playerElement);
+      // make button disabled after selecting and change the disabled button color
       button.setAttribute("disabled", true);
       if (button.disabled == true) {
         button.style.background = "gray";
@@ -22,15 +25,18 @@ for (const button of selectBtn1) {
     const playerExpenses = document.getElementById("player-expenses");
     calculateBtn.addEventListener("click", function (event) {
       event.preventDefault();
+      // calculate player expenses
       const playerExpensesAmount = perPlayer(selectedPlayersNumber);
       playerExpenses.innerText = playerExpensesAmount;
       const totalCalculateBtn = document.getElementById("total-calculate-btn");
       const totalExpensesText = document.getElementById("total-expenses");
-
       totalCalculateBtn.addEventListener("click", function (event) {
         event.preventDefault();
+        // calculate manager expenses
         const managerPrice = getValueFromId("manager-price");
+        // calculate coach expenses
         const coachPrice = getValueFromId("coach-price");
+        // total calculation of expenses
         const totalExpenses = playerExpensesAmount + managerPrice + coachPrice;
         totalExpensesText.innerText = totalExpenses;
       });
